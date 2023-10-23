fc8b2da82561b8b2c2a604b3928422bfeaa84097
Kawin <kawind7@gmail.com>
2023-10-23 21:28:35 +0700

    Async Await #1-6

--- ex-2.js
+++ ex-2.js
@@ -13,3 +13,10 @@ let getJohnProfile = () => {
 };
 
 // Start coding here
+getJohnProfile()
+  .then((profile) => {
+    console.log(profile);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
--- ex-5.js
+++ ex-5.js
@@ -12,3 +12,13 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+async function fetchData() {
+  try {
+    const result = await getJohnProfile();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
